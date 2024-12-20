14e5c585d3ee008820e2377587b9fac385b37532
Ferdnan Luniere <ferdnandluniere1@gmail.com>
2024-12-20 15:31:50 -0400

    Debug - treino

--- app.py
+++ app.py
@@ -1,1 +1,18 @@
-print('Olá, estamos no nosso primeiro projeto!')
+#​Código utilizado na aula:
+
+print('Olá')
+
+def calcular_preco_combo(pizza, refrigerante):
+
+    total = pizza + refrigerante
+    print(total)
+
+calcular_preco_combo(30, 20)
+print('Programa finalizado')
+
+#F9 Marca um breakpoint
+#F5 Abrir modo debug
+#F10 Passar sobre uma linha, SEM entrar em método interno
+#F11 Passar sobre uma linha e ENTRAR em método interno
+
+#Exercício de Debug 
